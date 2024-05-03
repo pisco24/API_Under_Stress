558f719c9c6d850598e7d4461de67c933e0b9957
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-03 16:23:34 -0700

    chore: create constants.hpp

---
+++ my_crow_mongo_app/inc/constants.hpp
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+constexpr char kMongoDbUri[] = "mongodb://db:27017";
+constexpr char kDatabaseName[] = "warrior_db";
+constexpr char kCollectionName[] = "warrior_info";
+
+#endif // CONSTANTS_HPP
