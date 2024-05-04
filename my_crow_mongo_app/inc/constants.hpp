12f6c23031583ea33b03332a563c2cc14a010b3b
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-04 10:12:03 -0700

    chore: add fight skill constants

--- my_crow_mongo_app/inc/constants.hpp
+++ my_crow_mongo_app/inc/constants.hpp
@@ -5,4 +5,32 @@ constexpr char kMongoDbUri[] = "mongodb://db:27017";
 constexpr char kDatabaseName[] = "test_warrior_db";
 constexpr char kCollectionName[] = "warrior_info";
 
+const std::unordered_set<std::string> valid_skills = {
+        "BJJ"
+        , "Karate"
+        , "Judo"
+        , "KungFu"
+        , "Capoeira"
+        , "Boxing"
+        , "Taekwondo"
+        , "Aikido"
+        , "KravMaga"
+        , "MuayThai"
+        , "KickBoxing"
+        , "Pankration"
+        , "Wrestling"
+        , "Sambo"
+        , "Savate"
+        , "Sumo"
+        , "Kendo"
+        , "Hapkido"
+        , "LutaLivre"
+        , "WingChu"
+        , "Ninjutsu"
+        , "Fencing"
+        , "ArmWrestling"
+        , "SuckerPunch"
+        , "44Magnum"
+    };
+
 #endif // CONSTANTS_HPP
