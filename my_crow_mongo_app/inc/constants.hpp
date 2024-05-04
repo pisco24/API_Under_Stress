1559fd0ca96da3293e3bbf73ed8d9678c4cace1e
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-03 20:41:45 -0700

    chore: create new test collection

--- my_crow_mongo_app/inc/constants.hpp
+++ my_crow_mongo_app/inc/constants.hpp
@@ -2,7 +2,7 @@
 #define CONSTANTS_HPP
 
 constexpr char kMongoDbUri[] = "mongodb://db:27017";
-constexpr char kDatabaseName[] = "warrior_db";
+constexpr char kDatabaseName[] = "test_warrior_db";
 constexpr char kCollectionName[] = "warrior_info";
 
 #endif // CONSTANTS_HPP
