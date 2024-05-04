39a82063b81bfdb495a32e1f3977c497e8cd123f
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-03 20:43:14 -0700

    remove mdb setup

--- my_crow_mongo_app/src/main.cpp
+++ my_crow_mongo_app/src/main.cpp
@@ -1,9 +1,8 @@
 #include "../inc/http_server.hpp"
-#include "../inc/mongodb_setup.hpp"
+// #include "../inc/mongodb_setup.hpp"
 
 int main()
 {
-  MongoDbSetup db_setup;
   HttpServer server;
   server.setupRoutes();
   server.run(18080);
